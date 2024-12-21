4788d645f220c3cfeeb6e3304822dba9d162265b
Ghufron Ainun Najib <ghufrnainunajib@gmail.com>
2024-12-21 07:04:17 +0700

    sedikit penambahan efek animasi dan hover

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function onToggleMenu(e) {
   e.name = e.name === "menu" ? "close" : "menu";
   navLinks.classList.toggle("top-0");
 }
+var typed = new Typed("#typed", {
+  strings: ["A Web Developer", "A Creative Thinker", "A Technology Enthusiast"],
+  typeSpeed: 50,
+  backSpeed: 25,
+  loop: true,
+});
